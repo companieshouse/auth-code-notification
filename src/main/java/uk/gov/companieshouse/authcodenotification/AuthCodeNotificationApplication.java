ce687b9273f55a111ddf40ece43370f4bf399a73
mattch1 <mculpin@companieshouse.gov.uk>
2023-04-04 14:42:36 +0100

    ROE-2104 Removed controller for now, use actuator instead

--- src/main/java/uk/gov/companieshouse/authcodenotification/AuthCodeNotificationApplication.java
+++ src/main/java/uk/gov/companieshouse/authcodenotification/AuthCodeNotificationApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthCodeNotificationApplication
-{
+public class AuthCodeNotificationApplication {
     public static void main( String[] args )
     {
         SpringApplication.run(AuthCodeNotificationApplication.class);
