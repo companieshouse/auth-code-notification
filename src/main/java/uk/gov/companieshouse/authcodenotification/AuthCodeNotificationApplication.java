e83c0d74e910517ac7808ef59c694988920fcb79
mattch1 <mculpin@companieshouse.gov.uk>
2023-04-04 13:42:17 +0100

    ROE-2104 Spring Boot application

---
+++ src/main/java/uk/gov/companieshouse/authcodenotification/AuthCodeNotificationApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.authcodenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCodeNotificationApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AuthCodeNotificationApplication.class);
+    }
+}
