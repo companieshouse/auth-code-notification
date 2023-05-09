42f3851fda8040aa887ad4772f6425fce42492f1
mattch1 <mculpin@companieshouse.gov.uk>
2023-05-09 14:17:13 +0100

    Roe-2196 Added logging and exception handling

--- src/main/java/uk/gov/companieshouse/authcodenotification/service/AuthCodeNotificationService.java
+++ src/main/java/uk/gov/companieshouse/authcodenotification/service/AuthCodeNotificationService.java
@@ -1,8 +1,11 @@
 package uk.gov.companieshouse.authcodenotification.service;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.authcodenotification.exception.ServiceException;
+import uk.gov.companieshouse.authcodenotification.utils.ApiLogger;
+import uk.gov.companieshouse.logging.util.DataMap;
 
 @Service
 public class AuthCodeNotificationService {
@@ -10,10 +13,19 @@ public class AuthCodeNotificationService {
     @Autowired
     private PrivateDataRetrievalService privateDataRetrievalService;
 
-    public void sendAuthCodeEmail(String companyNumber) throws ServiceException {
-        // todo add some logging
+    public void sendAuthCodeEmail(String requestId, String companyNumber) throws ServiceException {
+        DataMap dataMap = new DataMap.Builder().companyNumber(companyNumber).build();
+        ApiLogger.infoContext(requestId, "Send auth code email invoked", dataMap.getLogMap());
+
         String email = getOverseasEntityEmail(companyNumber);
 
+        if (Strings.isNotBlank(email)) {
+            ApiLogger.infoContext(requestId, "Retrieved auth code email successfully", dataMap.getLogMap());
+        } else {
+            ServiceException e = new ServiceException("Null or empty email found");
+            ApiLogger.errorContext(requestId, "Failed to retrieve complete email", e, dataMap.getLogMap());
+            throw e;
+        }
         // 2. send email
     }
 
