a97dee1ec8d2f2e79bf1669f324a38dd02be7f41
mattch1 <mculpin@companieshouse.gov.uk>
2023-05-09 13:25:46 +0100

    Roe-2200 Data sanitisation code

--- src/main/java/uk/gov/companieshouse/authcodenotification/utils/Constants.java
+++ src/main/java/uk/gov/companieshouse/authcodenotification/utils/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
 
     public static final String ERIC_REQUEST_ID_KEY = "X-Request-Id";
 
+    public static final int TRUNCATED_DATA_LENGTH = 50;
+
     private Constants() {}
 }
