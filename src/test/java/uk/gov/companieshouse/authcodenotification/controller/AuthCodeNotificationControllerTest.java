12587bed986f1725fd177ddcd0c67b63a3e0d1a8
mattch1 <mculpin@companieshouse.gov.uk>
2023-05-10 16:05:24 +0100

    Roe-2200 Fixed failed tests

--- src/test/java/uk/gov/companieshouse/authcodenotification/controller/AuthCodeNotificationControllerTest.java
+++ src/test/java/uk/gov/companieshouse/authcodenotification/controller/AuthCodeNotificationControllerTest.java
@@ -8,9 +8,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 import uk.gov.companieshouse.authcodenotification.exception.ServiceException;
 import uk.gov.companieshouse.authcodenotification.service.AuthCodeNotificationService;
+import uk.gov.companieshouse.authcodenotification.utils.DataSanitisation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AuthCodeNotificationControllerTest {
@@ -25,16 +27,20 @@ class AuthCodeNotificationControllerTest {
     @Mock
     private AuthCodeNotificationService authCodeNotificationService;
 
+    @Mock
+    private DataSanitisation dataSanitisation;
+
     @Test
     void testSendEmailReturnsSuccess() {
-        ResponseEntity<Object> responseEntity = controller.sendEmail(REQUEST_ID ,COMPANY_NUMBER);
+        ResponseEntity<Object> responseEntity = controller.sendEmail(REQUEST_ID, COMPANY_NUMBER);
         assertEquals( 200, responseEntity.getStatusCode().value() );
     }
 
     @Test
     void testSendEmailReturnsInternalServerErrorWhenServiceCallFails() throws ServiceException{
+        when(dataSanitisation.makeStringSafeForLogging(COMPANY_NUMBER)).thenReturn(COMPANY_NUMBER);
         doThrow(new ServiceException("")).when(authCodeNotificationService).sendAuthCodeEmail(REQUEST_ID , COMPANY_NUMBER);
-        ResponseEntity<Object> responseEntity = controller.sendEmail(REQUEST_ID ,COMPANY_NUMBER);
+        ResponseEntity<Object> responseEntity = controller.sendEmail(REQUEST_ID, COMPANY_NUMBER);
         assertEquals( 500, responseEntity.getStatusCode().value() );
     }
 }
