79633a6a78c0053b76b29906b49290657188fc92
mwestacott <mwestacott2@companieshouse.gov.uk>
2023-05-16 08:50:57 +0100

    ROE-2212 Simplify implementation
    * Deemed that sanitisation of output strings not required

--- src/main/java/uk/gov/companieshouse/authcodenotification/exception/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/companieshouse/authcodenotification/exception/GlobalExceptionHandler.java
@@ -1,9 +1,5 @@
 package uk.gov.companieshouse.authcodenotification.exception;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.owasp.encoder.Encode;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,35 +7,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import uk.gov.companieshouse.authcodenotification.utils.ApiLogger;
 
-import java.util.HashMap;
-
 import static uk.gov.companieshouse.authcodenotification.utils.Constants.ERIC_REQUEST_ID_KEY;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @Value("${GLOBAL_EXCEPTION_HANDLER_TRUNCATE_LENGTH_CHARS:15000}")
-    private int truncationLength;
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleException(Exception ex, WebRequest webRequest) {
 
         var context = webRequest.getHeader(ERIC_REQUEST_ID_KEY);
-        var sanitisedExceptionMessage = truncate(Encode.forJava(ex.getMessage()));
-        var sanitisedStackTrace = truncate(Encode.forJava(ExceptionUtils.getStackTrace(ex)));
-        var sanitisedRootCause = truncate(Encode.forJava(ExceptionUtils.getStackTrace(ExceptionUtils.getRootCause(ex))));
 
-        HashMap<String, Object> logMap = new HashMap<>();
-        logMap.put("error", ex.getClass());
-        logMap.put("stackTrace", sanitisedStackTrace);
-        logMap.put("rootCause", sanitisedRootCause);
-
-        ApiLogger.errorContext(context, sanitisedExceptionMessage, null, logMap);
+        ApiLogger.errorContext(context, "Unhandled exception", ex);
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    private String truncate(String input) {
-        return StringUtils.truncate(input, truncationLength);
-    }
-
 }
