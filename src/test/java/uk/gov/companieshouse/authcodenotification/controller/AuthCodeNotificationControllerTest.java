f30acde539c4fda884705b5a776bcd358651292c
mattch1 <mculpin@companieshouse.gov.uk>
2023-05-18 15:19:21 +0100

    Test coverage

--- src/test/java/uk/gov/companieshouse/authcodenotification/controller/AuthCodeNotificationControllerTest.java
+++ src/test/java/uk/gov/companieshouse/authcodenotification/controller/AuthCodeNotificationControllerTest.java
@@ -12,6 +12,7 @@ import uk.gov.companieshouse.authcodenotification.model.SendEmailRequestDto;
 import uk.gov.companieshouse.authcodenotification.service.AuthCodeNotificationService;
 import uk.gov.companieshouse.authcodenotification.utils.DataSanitiser;
 import uk.gov.companieshouse.authcodenotification.validation.AuthCodeEmailValidator;
+import uk.gov.companieshouse.service.rest.err.Err;
 import uk.gov.companieshouse.service.rest.err.Errors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -48,20 +49,34 @@ class AuthCodeNotificationControllerTest {
         sendEmailRequestDto.setAuthCode(AUTH_CODE);
         when(dataSanitiser.makeStringSafe(COMPANY_NUMBER)).thenReturn(COMPANY_NUMBER);
         when(dataSanitiser.makeStringSafe(AUTH_CODE)).thenReturn(AUTH_CODE);
-        Errors errors = new Errors();
-        when(authCodeEmailValidator.validate(eq(COMPANY_NUMBER), eq(AUTH_CODE), any(), eq(REQUEST_ID))).thenReturn(errors);
     }
 
     @Test
     void testSendEmailReturnsSuccess() {
+        Errors errors = new Errors();
+        when(authCodeEmailValidator.validate(eq(COMPANY_NUMBER), eq(AUTH_CODE), any(), eq(REQUEST_ID))).thenReturn(errors);
         ResponseEntity<Object> responseEntity = controller.sendEmail(REQUEST_ID, sendEmailRequestDto, COMPANY_NUMBER);
         assertEquals( 200, responseEntity.getStatusCode().value() );
     }
 
     @Test
     void testSendEmailReturnsInternalServerErrorWhenServiceCallFails() throws ServiceException {
+        Errors errors = new Errors();
+        when(authCodeEmailValidator.validate(eq(COMPANY_NUMBER), eq(AUTH_CODE), any(), eq(REQUEST_ID))).thenReturn(errors);
         doThrow(new ServiceException("")).when(authCodeNotificationService).sendAuthCodeEmail(REQUEST_ID, AUTH_CODE, COMPANY_NUMBER);
         ResponseEntity<Object> responseEntity = controller.sendEmail(REQUEST_ID, sendEmailRequestDto, COMPANY_NUMBER);
         assertEquals( 500, responseEntity.getStatusCode().value() );
+    }
+
+    @Test
+    void testSendValidationFailureReturnsBadRequest() {
+        Errors errors = new Errors();
+        final String errorLocation = "EXAMPLE_ERROR_LOCATION";
+        final String error = "EXAMPLE_ERROR";
+        Err err = Err.invalidBodyBuilderWithLocation(errorLocation).withError(error).build();
+        errors.addError(err);
+        when(authCodeEmailValidator.validate(eq(COMPANY_NUMBER), eq(AUTH_CODE), any(), eq(REQUEST_ID))).thenReturn(errors);
+        ResponseEntity<Object> responseEntity = controller.sendEmail(REQUEST_ID, sendEmailRequestDto, COMPANY_NUMBER);
+        assertEquals( 400, responseEntity.getStatusCode().value() );
     }
 }
