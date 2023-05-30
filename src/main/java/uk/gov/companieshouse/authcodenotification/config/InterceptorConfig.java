dd5b691286b6c5d93e85b0530243c1481ca2f10b
markpit <mpitten@companieshouse.gov.uk>
2023-05-30 13:58:26 +0100

    ROE-2234 Add InternalUserInterceptor from api-security-java to only allow internal api key requests

---
+++ src/main/java/uk/gov/companieshouse/authcodenotification/config/InterceptorConfig.java
@@ -0,0 +1,32 @@
+package uk.gov.companieshouse.authcodenotification.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
+
+@Configuration
+@ComponentScan("uk.gov.companieshouse.api.interceptor")
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    static final String ALL_PATHS = "/**";
+    static final String HEALTH_CHECK_PATH = "/auth-code-notification/healthcheck";
+
+    @Autowired
+    private InternalUserInterceptor internalUserInterceptor;
+
+    /**
+     * Set up the interceptors to run against endpoints when the endpoints are called
+     * Interceptors are executed in the order they are added to the registry
+     * @param registry The spring interceptor registry
+     */
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(internalUserInterceptor)
+                .addPathPatterns(ALL_PATHS)
+                .excludePathPatterns(HEALTH_CHECK_PATH);
+    }
+}
---
+++ src/test/java/uk/gov/companieshouse/authcodenotification/config/InterceptorConfigTest.java
@@ -0,0 +1,51 @@
+package uk.gov.companieshouse.authcodenotification.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class InterceptorConfigTest {
+
+    @Mock
+    private InterceptorRegistry interceptorRegistry;
+
+    @Mock
+    private InterceptorRegistration interceptorRegistration;
+
+    @Mock
+    private InternalUserInterceptor internalUserInterceptor;
+
+    @InjectMocks
+    private InterceptorConfig interceptorConfig;
+
+    @Test
+    void addInterceptorsTest() {
+        when(interceptorRegistry.addInterceptor(any())).thenReturn(interceptorRegistration);
+        when(interceptorRegistration.addPathPatterns(any(String.class))).thenReturn(interceptorRegistration);
+        when(interceptorRegistration.excludePathPatterns(any(String.class))).thenReturn(interceptorRegistration);
+
+        interceptorConfig.addInterceptors(interceptorRegistry);
+
+        //  The order that the interceptors are added is important, they get executed in the order they are added,
+        //  i.e. first interceptor added gets run first.
+        //  This test will ensure that the interceptors are added in the correct order in InterceptorConfig
+
+        InOrder inOrder = inOrder(interceptorRegistry, interceptorRegistration);
+
+        // Internal User authentication interceptor check
+        inOrder.verify(interceptorRegistry).addInterceptor(internalUserInterceptor);
+        inOrder.verify(interceptorRegistration).addPathPatterns(InterceptorConfig.ALL_PATHS);
+        inOrder.verify(interceptorRegistration).excludePathPatterns(InterceptorConfig.HEALTH_CHECK_PATH);
+    }
+}
