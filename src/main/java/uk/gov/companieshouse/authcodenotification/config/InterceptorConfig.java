5331dcc5b5eb5e5ff58a54a3412c4f32ec48ad6a
jbishop2-ch <jbishop2@companieshouse.gov.uk>
2026-03-18 09:46:08 +0000

    Added in logging and DI of config values

--- src/main/java/uk/gov/companieshouse/authcodenotification/config/InterceptorConfig.java
+++ src/main/java/uk/gov/companieshouse/authcodenotification/config/InterceptorConfig.java
@@ -7,6 +7,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
+import uk.gov.companieshouse.authcodenotification.utils.ApiLogger;
 
 @Configuration
 @ComponentScan("uk.gov.companieshouse.api.interceptor")
@@ -14,15 +15,16 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     private static final String ALL_PATHS = "/**";
 
-    @Value("${management.endpoints.web.base-path}")
-    private String managementBasePath;
-
-    @Value("${management.endpoints.web.path-mapping.health}")
-    private String healthCheckPathSuffix;
+    private final String managementBasePath;
+    private final String healthCheckPathSuffix;
 
     private final InternalUserInterceptor internalUserInterceptor;
 
-    public InterceptorConfig(InternalUserInterceptor interceptor) {
+    public InterceptorConfig(@Value("${management.endpoints.web.base-path}") String managementBasePath,
+            @Value("${management.endpoints.web.path-mapping.health}") String healthCheckPathSuffix,
+            InternalUserInterceptor interceptor) {
+        this.managementBasePath = managementBasePath;
+        this.healthCheckPathSuffix = healthCheckPathSuffix;
         this.internalUserInterceptor = interceptor;
     }
 
@@ -33,6 +35,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        ApiLogger.info("addInterceptors(managementBasePath=%s, healthCheckPathSuffix=%s) method called."
+                .formatted(managementBasePath, healthCheckPathSuffix));
+
         registry.addInterceptor(internalUserInterceptor)
                 .addPathPatterns(ALL_PATHS)
                 .excludePathPatterns(managementBasePath + healthCheckPathSuffix);
